d4f5d7b395c7a75a9e0dde6c9c8853a7cfee0082
Pawel Slapczynski <pawel.slapczynski@student.wat.edu.pl>
2020-06-18 09:28:58 +0200

    Wykonano zadanie 3.3.1 – tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
+#include <LiquidCristal.h>
 
-void setup() {  
+LiquidCeistal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() { 
+    lcd.begin(16, 2);
+    lcd.print("Słapczyński") ;
 }
 
 void loop() {  
+    lcd.setCursor(0, 1);
+    lcd.pront(millis() / 1000);
+    dealy(500);
 }
